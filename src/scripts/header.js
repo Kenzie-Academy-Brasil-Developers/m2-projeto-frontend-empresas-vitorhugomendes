d48d60120710406914e7280e07e9971ce5edaf72
Vitor Hugo Mendes <vitor_hugorj@hotmail.com>
2023-01-06 13:45:05 -0300

    pagina inicial feita

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -7,4 +7,20 @@ function showHideButtons() {
   });
 }
 
+function pageRedirectButtons() {
+  const buttons = document.querySelectorAll("button");
+
+  buttons.forEach((button) => {
+    if (button.innerText == "Login") {
+      button.addEventListener("click", () => {
+        window.location.replace("/src/pages/login.html");
+      });
+    } else if (button.innerText == "Cadastro") {
+      button.addEventListener("click", () => {
+        window.location.replace("/src/pages/register.html");
+      });
+    }
+  });
+}
+pageRedirectButtons();
 showHideButtons();
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,8 +4,54 @@ import {
   readCompaniesBySector,
 } from "./requests.js";
 
-console.log(await readAllCompanies());
+function renderCompaniesList(companies) {
+  const companiesList = document.querySelector(".company__list");
 
-console.log(await readAllCompaniesSectors());
+  companiesList.innerHTML = "";
 
-console.log(await readCompaniesBySector("Varejo"));
+  companies.forEach((company) => {
+    const companyCard = createCompanyCard(company);
+
+    companiesList.appendChild(companyCard);
+  });
+}
+
+function createCompanyCard({ name, opening_hours, sectors }) {
+  const companyCard = document.createElement("li");
+  const companyName = document.createElement("h2");
+  const companyOpeningHours = document.createElement("p");
+  const companySector = document.createElement("span");
+
+  companyCard.classList = "company__card";
+
+  companyName.innerText = name;
+  companyOpeningHours.innerText = opening_hours;
+  companySector.innerText = sectors.description;
+
+  companyCard.append(companyName, companyOpeningHours, companySector);
+
+  return companyCard;
+}
+
+function renderSelectOptions(sectors) {
+  const select = document.querySelector("#sector");
+
+  sectors.forEach((sector) => {
+    select.insertAdjacentHTML(
+      "beforeend",
+      `<option value=${sector.description}>${sector.description}</option>`
+    );
+  });
+}
+
+function renderSelectedCompanys() {
+  const select = document.querySelector("#sector");
+
+  select.addEventListener("change", async (e) => {
+    renderCompaniesList(await readCompaniesBySector(select.value));
+  });
+}
+
+renderCompaniesList(await readAllCompanies());
+renderSelectOptions(await readAllCompaniesSectors());
+renderSelectedCompanys();
